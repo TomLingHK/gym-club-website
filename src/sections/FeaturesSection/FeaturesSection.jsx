64d4eaa050a4833421594a0a010540afbd06604d
H251 Tom <251@solmaze.net>
2024-08-20 10:41:48 +0800

    dev: [FeaturesSection] Add feature items

--- src/sections/FeaturesSection/FeaturesSection.jsx
+++ src/sections/FeaturesSection/FeaturesSection.jsx
@@ -1,29 +1,60 @@
 import './FeaturesSection.scss'
 
 function FeaturesSection() {
-  return (
-    <section className='featuresSection'>
-        <h1>
-            Why Us?
-        </h1>
-        <div className='featureContainer'>
-            <div className='img'>
-                
-            </div>
-            <div className='text'>
-                <h1 className='title'>
-                    Work out safely
-                </h1>
-                <div className='description'>
-                    Try our free trial section from one of our 100+ classes
+    return (
+        <section className='featuresSection'>
+            <h1 className='mainTitle'>
+                Why Us?
+            </h1>
+            <div className='featureContainer'>
+                <div className='img'>
                 </div>
-                <div className='btn moreBtn'>
-                    More
+                <div className='text'>
+                    <h1 className='title'>
+                        Safety first
+                    </h1>
+                    <div className='description'>
+                        We provide professional trainers for every classes. Providing you a fruitful experience to learn in a correct manner. Follow our trainers' instructions to learn safely.
+                    </div>
+                    <div className='btn moreBtn'>
+                        More
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-  )
+
+            <div className='featureContainer'>
+                <div className='img'>
+                </div>
+                <div className='text'>
+                    <h1 className='title'>
+                        Flexible schedule
+                    </h1>
+                    <div className='description'>
+                        Feel free to join any sections of classes every week. Arrange your time more effectively.
+                    </div>
+                    <div className='btn moreBtn'>
+                        More
+                    </div>
+                </div>
+            </div>
+
+            <div className='featureContainer'>
+                <div className='img'>
+                </div>
+                <div className='text'>
+                    <h1 className='title'>
+                        100+ classes
+                    </h1>
+                    <div className='description'>
+                        We provide 100+ classes from begginer to advanced level. Gym, yoga, HIIT and sports you have never heard of. Check out what classes we provide!
+                    </div>
+                    <div className='btn moreBtn'>
+                        More
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default FeaturesSection;
