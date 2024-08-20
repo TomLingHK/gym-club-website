7ae35c27f05f1af1613c5688962d5512be9fe19a
H251 Tom <251@solmaze.net>
2024-08-20 14:29:55 +0800

    dev: [Feature] Add new component FeatureContainer

---
+++ src/components/FeatureContainer/FeatureContainer.jsx
@@ -0,0 +1,23 @@
+import './FeatureContainer.scss'
+
+function FeatureContainer({ id, title, description }) {
+    return (
+        <div className={'featureContainer ' + id}>
+            <div className='img'>
+            </div>
+            <div className='text'>
+                <h1 className='title'>
+                    { title }
+                </h1>
+                <div className='description'>
+                    { description }
+                </div>
+                <div className='btn moreBtn'>
+                    More
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeatureContainer;
--- src/sections/FeaturesSection/FeaturesSection.jsx
+++ src/sections/FeaturesSection/FeaturesSection.jsx
@@ -1,57 +1,22 @@
+import FeatureContainer from '../../components/FeatureContainer/FeatureContainer';
 import './FeaturesSection.scss'
 
 function FeaturesSection() {
+    const featureInfo = [
+        { id: 'feature1', title: 'Safety first', description: 'We provide professional trainers for every classes. Providing you a fruitful experience to learn in a correct manner. Follow our trainers\' instructions to learn safely.' },
+        { id: 'feature2', title: 'Flexible schedule', description: 'There are at least three time slots for each class every week. Feel free to join any section every week. Arrange your time more effectively.' },
+        { id: 'feature3', title: '100+ classes', description: 'We provide 100+ classes from beginner to advanced level. Gym, yoga, HIIT and sports you have perhaps never heard of. Check out what classes we provide!' },
+    ];
+
     return (
         <section className='featuresSection'>
             <h1 className='mainTitle'>
                 Why Us?
             </h1>
-            <div className='featureContainer feature1'>
-                <div className='img'>
-                </div>
-                <div className='text'>
-                    <h1 className='title'>
-                        Safety first
-                    </h1>
-                    <div className='description'>
-                        We provide professional trainers for every classes. Providing you a fruitful experience to learn in a correct manner. Follow our trainers' instructions to learn safely.
-                    </div>
-                    <div className='btn moreBtn'>
-                        More
-                    </div>
-                </div>
-            </div>
-
-            <div className='featureContainer feature2'>
-                <div className='img'>
-                </div>
-                <div className='text'>
-                    <h1 className='title'>
-                        Flexible schedule
-                    </h1>
-                    <div className='description'>
-                        There are at least three time slots for each class every week. Feel free to join any section every week. Arrange your time more effectively.
-                    </div>
-                    <div className='btn moreBtn'>
-                        More
-                    </div>
-                </div>
-            </div>
-
-            <div className='featureContainer feature3'>
-                <div className='img'>
-                </div>
-                <div className='text'>
-                    <h1 className='title'>
-                        100+ classes
-                    </h1>
-                    <div className='description'>
-                        We provide 100+ classes from beginner to advanced level. Gym, yoga, HIIT and sports you have perhaps never heard of. Check out what classes we provide!
-                    </div>
-                    <div className='btn moreBtn'>
-                        More
-                    </div>
-                </div>
+            <div className='featuresContainer'>
+                {featureInfo.map(({ id, title, description }) => 
+                    <FeatureContainer key={id} id={id} title={title} description={description} />
+                )}
             </div>
         </section>
     )
