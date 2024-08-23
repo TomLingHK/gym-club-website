c60b311a9a70d2940d676af3997718055a92ba9b
H251 Tom <251@solmaze.net>
2024-08-23 17:27:47 +0800

    dev: [Styling] Add fade animation for feedback section

--- src/components/FeedbackContainer/FeedbackContainer.jsx
+++ src/components/FeedbackContainer/FeedbackContainer.jsx
@@ -1,28 +1,42 @@
+import { useEffect, useState } from 'react';
 import './FeedbackContainer.scss'
 
 function FeedbackContainer({ highlightContent, content, person, id }) {
+    const [fading, setFading] = useState(false);
+    let timeoutID = undefined;
+
+    useEffect(() => {
+        clearTimeout(timeoutID);
+        timeoutID = undefined;
+        setFading(true);
+
+        timeoutID = setTimeout(() => {
+            setFading(false);
+        }, 1000);
+    }, [id])
+
     return (
-        <div className='feedbackContainer'>
+        <div className={'feedbackContainer '}>
             <div className='txt'>
                 <div className='highlightContainer'>
                     <h1 className='openQuotationMark'>
                         “
                     </h1>
-                    <h3 className='highlightContent'>
+                    <h3 className={'highlightContent' + (fading ? ' fading' : '')}>
                         { highlightContent }
                     </h3>
                     <h1 className='closeQuotationMark'>
                         ”
                     </h1>
                 </div>      
-                <div className='content'>
+                <div className={'content ' + (fading ? 'fading' : '')}>
                     { content }
                 </div>
-                <div className='name'>
+                <div className={'name ' + (fading ? 'fading' : '')}>
                     - { person }
                 </div>
             </div>
-            <img src={`./images/${id}_img.jpg`} alt="Happy person" className='img' />
+            <img src={`./images/${id}_img.jpg`} alt="Happy person" className={'img ' + (fading ? 'fading' : '')} />
         </div>
     )
 }
