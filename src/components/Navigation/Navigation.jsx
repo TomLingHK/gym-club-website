6a5484c08e612484fb533a68ccb57a55a0c1554e
H251 Tom <251@solmaze.net>
2024-08-26 17:28:39 +0800

    dev: [NavBar] Add portrait styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import './Navigation.scss';
 import { useNavigate } from 'react-router-dom';
 
 function Navigation() {
     const navigate = useNavigate();
     const navItems = ['Home', 'Our Classes', 'Our Trainers', "HKGC"];
+    const [isPortrait, setIsPortrait] = useState(undefined);
+
+    useEffect(() => {
+        window.matchMedia("(orientation: portrait)").addEventListener("change", e => {
+            const portrait = e.matches;
+        
+            if (portrait) {
+                setIsPortrait('portrait');
+            } else {
+                setIsPortrait('landscape');
+                
+            }
+        });
+
+        if (isPortrait == undefined){
+            window.matchMedia("(orientation: portrait)").matches ? setIsPortrait('portrait') : setIsPortrait('landscape');
+        }
+    }, [])
+    
 
     function onNavClick($item) {
         switch ($item) {
@@ -16,15 +36,17 @@ function Navigation() {
     }
 
     return (
-        <div className='navBar'>
+        <div className={'navBar ' + (isPortrait=='portrait' ? 'portrait' : 'landscape')}>
             <div className='logo'>
                 Logo
             </div>
+            {isPortrait=='landscape' &&
             <div className='navContainer'>
                 {navItems.map(item => 
                     <div className='navItem' key={item} onClick={ () => onNavClick(item) }>{item}</div> 
                 )}
             </div>
+            }
         </div>
     )
 }
