c9c59986c3225e586b6ab5337b66bcfee263a84a
TOM-GIGABYTE\user <bxtom0@gmail.com>
2024-11-18 21:44:44 +0800

    Set up class sections content #2

--- src/sections/ClassSection/GymClasses/GymClasses.jsx
+++ src/sections/ClassSection/GymClasses/GymClasses.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function GymClasses() {
-    const classesArr = ['Beginner programme', 'Barbell programme', 'Dumbell programme', 'Advanced weight lifting'];
+    const classesArr = ['Beginner programme', 'Barbell programme', 'Dumbell programme', 'Advanced weight lifting', 'Spinning Classes', 'Personal Trainer', 'Cardio Programme', 'Full-body workout', 'Core Programme'];
 
     return (
         <div>GymClasses</div>
--- src/sections/ClassSection/YogaClasses/YogaClasses.jsx
+++ src/sections/ClassSection/YogaClasses/YogaClasses.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function YogaClasses() {
-    const classesArr = ['Yoga for beginners', 'Aerial for beginners', 'Aerial Hoop', 'Pilates'];
+    const classesArr = ['Yoga for beginners', 'Aerial for beginners', 'Aerial Hoop', 'Pilates', 'Yoga Therapy', 'Yoga Wheel', 'Wall Rope', 'Mat Pilates'];
 
     return (
         <div>YogaClasses</div>
